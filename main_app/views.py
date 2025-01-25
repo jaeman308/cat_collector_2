257ddd9b89ea2913b64b4432c18e5aa411c384b6
jborruel308@me.com <jborruel308@me.com>
2025-01-25 13:15:06 -0800

    detial page complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
+from .models import Cat
 
-
-# Create your views here.
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
 
 def home(request):
     return render(request, 'home.html')
@@ -25,4 +9,9 @@ def about(request):
     return render(request, 'about.html')
 
 def cat_index(request):
+    cats = Cat.objects.all()  # look familiar?
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
